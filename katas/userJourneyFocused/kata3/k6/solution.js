77b05c24a966dd81be059cb25648a7cb5f0453b7
Mateusz Stachurzewski <mateusz.stachurzewski@netguru.com>
2022-09-30 09:44:49 +0200

    Added solution for /userJourneyFocused/kata3/k6

--- katas/userJourneyFocused/kata3/k6/solution.js
+++ katas/userJourneyFocused/kata3/k6/solution.js
@@ -1,15 +1,62 @@
 import http from 'k6/http';
-import {
-    group
-} from 'k6';
+import { uuidv4 } from 'https://jslib.k6.io/k6-utils/1.4.0/index.js';
 
-export const options = {
-    vus: 1,
-    iterations: 2
-    // duration: '60s'
+const baseUrl = 'http://localhost:3000/api/v1/booksApp'
+const name = `${uuidv4()}`
+const email = `${uuidv4()}@gmail.com`
+const password = 'TestPassword'
+
+const createAccount = () => {
+    const url = baseUrl + '/register'
+    const payload = JSON.stringify(
+        {
+            "name": name,
+            "email": email,
+            "password": password,
+            "language": "English",
+            "country": "Poland",
+            "currency": "USD"
+        }
+    )
+    const params = {
+        headers: {
+            'Content-Type': 'application/json',
+        }
+    };
+
+    const res = http.post(url, payload, params)
+    // console.log(res.status)
+    // console.log(res.body)
+    try {
+        return res.json()
+    } catch (err) {
+        return null
+    }
 };
 
-const baseUrl = 'http://localhost:3000/api/v1/booksApp/'
+const login = () => {
+    const url = baseUrl + '/login'
+    const payload = JSON.stringify(
+        {
+            "email": email,
+            "password": password
+        }
+    )
+    const params = {
+        headers: {
+            'Content-Type': 'application/json',
+        }
+    };
+
+    const res = http.post(url, payload, params)
+    // console.log(res.status)
+    // console.log(res.body)
+    try {
+        return res.json()
+    } catch (err) {
+        return null
+    }
+};
 
 const goToMainPage = () => {
     const url = baseUrl + '/books'
@@ -92,28 +139,42 @@ const submitOrder = (basketId) => {
     return res
 };
 
+const executeOnceUtil = (_function) => {
+    if ( __ITER === 0) {
+        _function()
+    }
+}
+
 export default function() {
-    let bookId;
-    group('Find a book', () => {
+
+    // === Workaround for running once per VU ===
+    const oncePerVUFunction = () => {
         goToMainPage();
-        const res = searchForBookByName()
+        createAccount();
+    };
+
+    executeOnceUtil(oncePerVUFunction);
+    // === Workaround for running once per VU ===
+
+    let bookId;
+
+    goToMainPage();
+
+    login();
+    goToMainPage();
+    const res = searchForBookByName()
+    if (res) {
+        bookId = res._id;
+    }
+    if (bookId) {
+        goToBooksDetails(bookId);
+        addBookToBasket(bookId);
+    }
+    if (bookId) {
+        const res = goToBasket()
         if (res) {
-            bookId = res._id;
+            const basketId = res._id
+            submitOrder(basketId);
         }
-    })
-    group('Add to basket', () => {
-        if (bookId) {
-            goToBooksDetails(bookId);
-            addBookToBasket(bookId);
-        }
-    })
-    group('Buy a book', () => {
-        if (bookId) {
-            const res = goToBasket()
-            if (res) {
-                const basketId = res._id
-                submitOrder(basketId);
-            }
-        }
-    })
+    }
 }
